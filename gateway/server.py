86ba2772007741adf0cbeefe27a9a630f37ad4db
Daryl Wilding-McBride <dwm@dius.com.au>
2013-10-04 11:22:36 +1000

    Now pushes sample values into i.li

--- gateway/server.py
+++ gateway/server.py
@@ -6,22 +6,40 @@ import struct
 from array import array
 from datetime import datetime, date, time
 import sys
+import time
+import ilififowriter
 
 def ByteToHex(byteStr): 
 	# Convert a byte string to it's hex string representation e.g. for output.
 	return ''.join( [ "%02X" % ord( x ) for x in byteStr ] ).strip() 
 
-ser = serial.Serial('/dev/tty.usbserial-AD01SS0B', 9600)
+ser = serial.Serial('/dev/ttyUSB0', 9600)
 xbee = ZigBee(ser, escaped=True)
 
-# Continuously read and print packets
-print 'Continuously read and print packets'
+# Set up the Intelligent.li FIFO writer
+ilififowriter.init_fifo('/dev/mmcblk0p6')
+collectionID = 'sensorMotes'
+
+# Continuously listen for sensor mote packets and send them to Intelligent.li
+print 'Continuously read packets and send them to Intelligent.li'
 while True:
 	try:
 		response = xbee.wait_read_frame()
 		msgType = struct.unpack("B", response['rf_data'][0])[0]
 		if msgType == 0xf3:
-			print 'info received from ' + ByteToHex(response['source_addr_long']) + ': ' + response['rf_data'][1:]
+			sourceAddress = ByteToHex(response['source_addr_long'])
+			sensorString = response['rf_data'][1:]
+			print 'info received from ' + sourceAddress + ': ' + sensorString
+			sampleTimestamp = int(time.time())
+			deviceID = response['source_addr_long']
+			# parse the string we received from the sensor mote
+			names = sensorString.split("&")[0].split("=")[1].split(",")
+			values = sensorString.split("&")[1].split("=")[1].split(",")
+			for idx, val in enumerate(names):
+				sensorID = val
+				sensorValue = float(values[idx].strip(' '))
+				print 'storing: ' + sensorID + '=' + '%f' % sensorValue
+				ilififowriter.store_sample(sampleTimestamp, collectionID, deviceID, sensorID, sensorValue)
 		else:
 			print 'not a recognised message type.'
 	except KeyboardInterrupt:
